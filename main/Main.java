10e09323f5224bca4f90345fc8478127f0b37feb
João Victor Noronha Cunha Santiago <jvictor99@hotmail.com>
2024-06-20 22:34:53 -0300

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     int opcao = 0;
 
-    while (opcao != 7) {
+    while (opcao != 8) {
       menu();
       opcao = scanner.nextInt();
       System.out.println("\n");
@@ -24,7 +24,11 @@ public class Main {
           listarPastas(gerenciador);
           break;
         case 3:
-          removerPasta(gerenciador, scanner);
+          try {
+            removerPasta(gerenciador, scanner);
+          } catch (PastaNaoEncontradaException e) {
+            System.out.println(e.getMessage());
+          }
           break;
         case 4:
           adicionarTarefa(gerenciador, scanner);
@@ -33,9 +37,16 @@ public class Main {
           gerenciador.listarTodasTarefas();
           break;
         case 6:
-          removerTarefa(gerenciador, scanner);
+          try {
+            removerTarefa(gerenciador, scanner);
+          } catch (PastaNaoEncontradaException e) {
+            System.out.println(e.getMessage());
+          }
           break;
         case 7:
+          editarTarefa(gerenciador, scanner);
+          break;
+        case 8:
           System.out.println("Saindo do programa.");
           break;
         default:
@@ -57,7 +68,8 @@ public class Main {
     System.out.println("4 - Adicionar Tarefa");
     System.out.println("5 - Listar Tarefas");
     System.out.println("6 - Remover Tarefa");
-    System.out.println("7 - Sair" + "\n");
+    System.out.println("7 - Alterar Status de Tarefa");
+    System.out.println("8 - Sair" + "\n");
     System.out.println("---------------");
     System.out.print("Escolha uma opção: " + "\n");
   }
@@ -76,7 +88,8 @@ public class Main {
     }
   }
 
-  public static void removerPasta(GerenciadorDeTarefas gerenciador, Scanner scanner) {
+  public static void removerPasta(GerenciadorDeTarefas gerenciador, Scanner scanner)
+      throws PastaNaoEncontradaException {
     System.out.println("Digite o nome da pasta que deseja remover");
     String nomePastaRemover = scanner.nextLine();
     gerenciador.removerPasta(nomePastaRemover);
@@ -89,7 +102,8 @@ public class Main {
     gerenciador.adicionarTarefa(nomePastaAdicionar, tarefa);
   }
 
-  public static void removerTarefa(GerenciadorDeTarefas gerenciador, Scanner scanner) {
+  public static void removerTarefa(GerenciadorDeTarefas gerenciador, Scanner scanner)
+      throws PastaNaoEncontradaException {
     System.out.println("Digite o nome da pasta onde deseja remover a tarefa: ");
     String nomePastaRemover = scanner.nextLine();
     System.out.println("Digite o ID da tarefa a ser removida: ");
@@ -97,24 +111,31 @@ public class Main {
     gerenciador.removerTarefa(nomePastaRemover, idRemovido);
   }
 
-  public static Tarefas preencheTarefa(Scanner scanner){
+  public static void editarTarefa(GerenciadorDeTarefas gerenciador, Scanner scanner) {
+    System.out.println("Digite o nome da pasta onde deseja alterar o status da tarefa: ");
+    String nomePastaEditar = scanner.nextLine();
+    System.out.println("Digite o ID da tarefa a ser alterado: ");
+    int idTarefa = scanner.nextInt();
+    scanner.nextLine();
+    try {
+      gerenciador.editarStatus(nomePastaEditar, idTarefa);
+    } catch (PastaNaoEncontradaException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  public static Tarefas preencheTarefa(Scanner scanner) {
     System.out.println("Digite o título da tarefa: ");
     String tituloTarefa = scanner.nextLine();
     System.out.println("Digite a descrição da tarefa: ");
     String descricaoTarefa = scanner.nextLine();
     System.out.println("Digite o status da tarefa (Concluida/Pendente): ");
     String status = scanner.nextLine();
-    while(!status.equalsIgnoreCase("Concluida") && !status.equalsIgnoreCase("Pendente")){
+    while (!status.equalsIgnoreCase("Concluida") && !status.equalsIgnoreCase("Pendente")) {
       System.out.println("Status inválido. Digite novamente: ");
       status = scanner.nextLine();
     }
-    Boolean statusTarefa = false;
-    if (status.equalsIgnoreCase("Concluida")) {
-      statusTarefa = true;
-    }
-    else{
-      statusTarefa = false;
-    }
+    Boolean statusTarefa = status.equalsIgnoreCase("Concluida");
     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     Date dataInicioTarefa = null;
     System.out.println("Digite a data de início da tarefa (dd/MM/yyyy): ");
@@ -132,11 +153,9 @@ public class Main {
     }
     System.out.println("Digite a prioridade da tarefa: \n" + "1 - Alta \n" + "2 - Media \n" + "3 - Baixa");
     int prioridadeTarefa = scanner.nextInt();
-    Tarefas tarefa = new Tarefas(tituloTarefa, descricaoTarefa, statusTarefa, dataInicioTarefa, prazoTarefa, prioridadeTarefa);
+    scanner.nextLine(); // Limpar o buffer após o nextInt()
+    Tarefas tarefa = new Tarefas(tituloTarefa, descricaoTarefa, statusTarefa, dataInicioTarefa, prazoTarefa,
+        prioridadeTarefa);
     return tarefa;
   }
-
 }
-
-
-//ADICIONAR UM EDITAR TAREFAS

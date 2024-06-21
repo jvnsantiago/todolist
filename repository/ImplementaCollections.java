acf04c1a23cce017886959cda1db5a7a100793b5
João Victor Noronha Cunha Santiago <jvictor99@hotmail.com>
2024-06-20 22:34:24 -0300

    Update ImplementaCollections.java

--- repository/ImplementaCollections.java
+++ repository/ImplementaCollections.java
@@ -1,23 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class ImplementaCollections<T> implements InterfaceBanco<T> {
+public class ImplementaCollections<T extends Comparable<T>> implements InterfaceBanco<T> {
     private List<T> data;
 
     public ImplementaCollections() {
         this.data = new ArrayList<>();
     }
-
     @Override
     public void save(T entity) {
         data.add(entity);
     }
-
     @Override
     public void delete(T entity) {
         data.remove(entity);
     }
-
     @Override
     public void update(T entity) {
         // Implementar lógica de atualização conforme necessário
@@ -25,6 +23,8 @@ public class ImplementaCollections<T> implements InterfaceBanco<T> {
 
     @Override
     public List<T> findAll() {
-        return data;
+        return data.stream()
+                   .sorted()
+                   .collect(Collectors.toList());
     }
 }
